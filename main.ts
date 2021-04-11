df9e5249af6336b312c6f5bd92597850288db644
Khoboyard <65360968+Khoboyard@users.noreply.github.com>
2021-04-11 14:27:32 +0200

    ajout d'un timer qui retire une loupiotte de manière aléatoire toutes les 3 à 7 secondes

--- main.ts
+++ main.ts
@@ -20,9 +20,6 @@ input.onGesture(Gesture.Shake, function () {
     Y = 2
     X = 2
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-	
-})
 input.onGesture(Gesture.LogoDown, function () {
     if (Y > 0) {
         led.unplot(X, Y)
@@ -38,8 +35,24 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
+let verifie_heure = input.runningTime()
 Y = 2
 X = 2
+let kill_pixel_every = randint(3000, 7000)
 basic.forever(function () {
     led.plot(X, Y)
+})
+basic.forever(function () {
+    control.waitMicros(5000000)
+    while (true) {
+        control.waitMicros(randint(1000000, 80000000))
+        led.unplot(randint(0, 4), randint(0, 4))
+    }
+})
+control.inBackground(function () {
+    if (verifie_heure + kill_pixel_every > input.runningTime()) {
+        led.unplot(randint(0, 4), randint(0, 4))
+        verifie_heure = input.runningTime()
+        kill_pixel_every = randint(3000, 7000)
+    }
 })
